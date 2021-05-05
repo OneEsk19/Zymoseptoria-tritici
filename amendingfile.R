af0d7d930b0773fc6e65b00bd217dfda841f8bc5
Georgina <50850809+OneEsk19@users.noreply.github.com>
2021-05-05 09:25:27 +0100

    Add files via upload

---
+++ amendingfile.R
@@ -0,0 +1,11 @@
+## trying to replace multiple "FILTERx" with just one "FILETERED"
+
+thefile <- tabfile
+
+thefile$FILTER[thefile$FILTER != "PASS"] <- "FILTERED" 
+head(thefile)
+
+
+thefile$FILTER <- as.character(thefile$FILTER)
+thefile$FILTER[is.na(thefile$FILTER)] <- "FILTERED"
+write.csv(thefile, "amemdedTABfile.tsv", sep = "/t")
